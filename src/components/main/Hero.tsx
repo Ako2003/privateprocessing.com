02eb9ca323b11d9813831e43e383299939fbc496
Ako2003 <akifmursalov@gmail.com>
2025-09-15 11:21:13 +0200

    Hero section is ready

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -7,23 +7,23 @@ const marquee_elements = ["No Blocked Accounts", "Dedicated Processing Agent", "
 
 export default function Hero() {
     return (
-        <section className="relative">
-            <div className="absolute -z-10 -translate-y-30 2xl:-translate-y-50">
+        <section className="relative z-10">
+            <div className="absolute translate-y-20 lg:-translate-y-30 2xl:-translate-y-50">
                 <Image src={"/svg/backgrounds/bg.svg"} alt={"Background Image"} width={2272} height={1419}/>
             </div>
             <div className="absolute -z-1 -translate-y-30 right-0 h-[calc(100vh+200px)] w-full">
                 <Image src={"/svg/backgrounds/bg1.svg"} alt={"Background Image"} fill/>
-                <div className="absolute bottom-0 h-[709px] w-full" style={{
+                <div className="absolute bottom-40 h-[709px] w-full" style={{
                     background: "linear-gradient(180deg, rgba(10, 10, 10, 0) 11.27%, rgba(10, 10, 10, 0.9) 81.87%, rgba(0, 0, 0, 0.95) 95.56%)",
                 }}/>
             </div>
-            <div className="absolute -z-30 -translate-y-30 right-0">
+            <div className="absolute -z-30 lg:-translate-y-30 translate-y-30 right-0">
                 <Image src={"/svg/backgrounds/golden-circle.svg"} alt={"Golden Circle"} width={606} height={583}/>
             </div>
             <div className="absolute -z-10 -bottom-60 left-1/2">
                 <Image src={"/svg/backgrounds/golden-circle.svg"} alt={"Golden Circle"} width={606} height={583}/>
             </div>
-            <div className="h-screen flex flex-col justify-between">
+            <div className="h-[calc(100svh)] flex flex-col justify-between">
                 <div className="mx-auto w-11/12 h-full">
                     <div className="flex flex-col justify-center h-full max-w-[824px]">
                         <h1>
