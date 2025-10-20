836dd9a513d59906e42bb6223826706a7409bfbb
Ako2003 <akifmursalov@gmail.com>
2025-10-20 10:08:47 +0200

    feat: Microsoft Clarity and Google Tag added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,41 +48,70 @@ export default function RootLayout({
                                    }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
-        <body className={`${manpore.variable} ${montserrat.variable} ${inter.variable} antialiased`}>
-        <Script
-            id="calendly-sdk"
-            src="https://assets.calendly.com/assets/external/widget.js"
-            strategy="afterInteractive"
-        />
-        {/* Load Facebook Pixel after hydration */}
-        <Script id="facebook-pixel" strategy="afterInteractive">
-            {`
-                      !function(f,b,e,v,n,t,s)
-                      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                      n.queue=[];t=b.createElement(e);t.async=!0;
-                      t.src=v;s=b.getElementsByTagName(e)[0];
-                      s.parentNode.insertBefore(t,s)}(window, document,'script',
-                      'https://connect.facebook.net/en_US/fbevents.js');
-                      fbq('init', '719123840945330');
-                      fbq('track', 'PageView');
-                    `}
-        </Script>
-        <noscript>
-            <img
-                height="1"
-                width="1"
-                style={{ display: "none" }}
-                src="https://www.facebook.com/tr?id=719123840945330&ev=PageView&noscript=1"
-            />
-        </noscript>
+            <body className={`${manpore.variable} ${montserrat.variable} ${inter.variable} antialiased`}>
 
-        <Navbar />
-        {children}
-        <Footer />
+                {/* Calendly SDK */}
+                <Script
+                    id="calendly-sdk"
+                    src="https://assets.calendly.com/assets/external/widget.js"
+                    strategy="afterInteractive"
+                />
 
-        </body>
+                {/* Google Tag (gtag.js) */}
+                <Script
+                    id="google-analytics"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-6TT5NVE8DQ"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics-init" strategy="afterInteractive">
+                    {`
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+                                gtag('config', 'G-6TT5NVE8DQ');
+                            `}
+                </Script>
+
+                {/* Facebook Pixel */}
+                <Script id="facebook-pixel" strategy="afterInteractive">
+                    {`
+                                !function(f,b,e,v,n,t,s)
+                                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                                n.queue=[];t=b.createElement(e);t.async=!0;
+                                t.src=v;s=b.getElementsByTagName(e)[0];
+                                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                                'https://connect.facebook.net/en_US/fbevents.js');
+                                fbq('init', '719123840945330');
+                                fbq('track', 'PageView');
+                            `}
+                </Script>
+                <noscript>
+                    <img
+                        height="1"
+                        width="1"
+                        style={{ display: "none" }}
+                        src="https://www.facebook.com/tr?id=719123840945330&ev=PageView&noscript=1"
+                    />
+                </noscript>
+
+                {/* Microsoft Clarity */}
+                <Script id="microsoft-clarity" strategy="afterInteractive">
+                    {`
+                                (function(c,l,a,r,i,t,y){
+                                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                                })(window, document, "clarity", "script", "r7leh0nkbz");
+                            `}
+                </Script>
+
+                {/* Site Content */}
+                <Navbar />
+                    {children}
+                <Footer />
+            </body>
         </html>
     );
 }
