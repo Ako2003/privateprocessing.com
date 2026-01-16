981173087e15e5d80b7ed2032e89b2fa61a58408
Ako2003 <akifmursalov@gmail.com>
2026-01-16 14:31:19 +0100

    Section 4 is ready

--- src/components/main/Section4.tsx
+++ src/components/main/Section4.tsx
@@ -6,53 +6,38 @@ export default function Section4(){
     return(
         <section className="lg:pt-30 lg:mt-20 pt-35" id="private-processing-solution">
             <div className="mx-auto max-w-[1200px] w-11/12 overflow-hidden">
-                <div>
-                    <ArrowText title={"Private Processing™ Solution"} className="lg:justify-center"/>
-                    <h3 className="lg:text-center lg:!text-[38px] capitalize leading-tight">
-                        <span className="text-gold font-bold capitalize">Time for the best.</span>
-                        <br/>
-                        Time for Private Processing
-                        <span className="align-super text-lg ml-0.5">™</span>
-                    </h3>
-
-                    <p className="lg:text-center lg:mx-auto max-w-[950px]  mt-5 text-lg mb-10">If you never want to face payment processing issues again
-                        and process 1M–100M per month with stability and peace of mind, the first step is to switch from
-                        Retail Processing to Private Processing™.</p>
-                </div>
-
                 <div className="flex lg:flex-row flex-col-reverse items-center justify-between max-lg:mt-10">
-                    <div className="lg:w-[450px]">
+                    <div className="lg:w-[700px]">
                         <div>
-                            <h3 className="!font-semibold !text-[28px]">What is retail processing?</h3>
-                            <p className="font-inter font-light text-whitisch mt-3">
-                                Retail processors (Shopify Payments, Stripe, PayPal, Airwallex, etc.) are the
-                                supermarkets of payment processing.
+                            <h3 className="!font-normal !text-[44px]">Looking for a <span className="!font-medium text-gold">specific local payment</span> option?</h3>
+                            <p className="font-inter font-semibold text-white mt-3">
+                                → We have it. You can choose it.
                             </p>
-                            <p className="font-inter font-light text-whitisch mt-3">
-                                They serve everyone but no one gets treated well.
+                            <p className="text-[#ACAFB9] mt-3">
+                                Filter payment processors based on local payment options and find processors that perfectly fit your needs.
                             </p>
                         </div>
-                        <hr className="border-t border-[#2b2b2b] w-full my-5"/>
-                        <div>
-                            <h3 className="!font-semibold !text-[28px]">What are private processors?</h3>
-                            <p className="font-inter font-light text-whitisch mt-3">
-                                Private processors are the private banks of payment processing and e-com; whales
-                                leverage them to process with peace of mind.
-                            </p>
-                            <p className="font-inter font-light text-whitisch mt-3">
-                                Instead of treating you like a number in the system, private processors build a
-                                long-term business relationship with you that helps you to grow stress-free.
-                            </p>
-                        </div>
-                        <hr className="border-t border-[#2b2b2b] w-full my-5"/>
-                        <div>
-                            <h3 className="!font-semibold !text-[28px]">Their number #1 priority?</h3>
-                            <p className="font-inter font-light text-whitisch mt-3">
-                                Making sure you can process payments. Because the more you process the more they earn.
-                            </p>
-                        </div>
-                        <hr className="border-t border-[#2b2b2b] w-full mt-5"/>
-                        <CustomButton className="mt-8" text="Start for free"/>
+                        {/*<hr className="border-t border-[#2b2b2b] w-full my-5"/>*/}
+                        {/*<div>*/}
+                        {/*    <h3 className="!font-semibold !text-[28px]">What are private processors?</h3>*/}
+                        {/*    <p className="font-inter font-light text-whitisch mt-3">*/}
+                        {/*        Private processors are the private banks of payment processing and e-com; whales*/}
+                        {/*        leverage them to process with peace of mind.*/}
+                        {/*    </p>*/}
+                        {/*    <p className="font-inter font-light text-whitisch mt-3">*/}
+                        {/*        Instead of treating you like a number in the system, private processors build a*/}
+                        {/*        long-term business relationship with you that helps you to grow stress-free.*/}
+                        {/*    </p>*/}
+                        {/*</div>*/}
+                        {/*<hr className="border-t border-[#2b2b2b] w-full my-5"/>*/}
+                        {/*<div>*/}
+                        {/*    <h3 className="!font-semibold !text-[28px]">Their number #1 priority?</h3>*/}
+                        {/*    <p className="font-inter font-light text-whitisch mt-3">*/}
+                        {/*        Making sure you can process payments. Because the more you process the more they earn.*/}
+                        {/*    </p>*/}
+                        {/*</div>*/}
+                        {/*<hr className="border-t border-[#2b2b2b] w-full mt-5"/>*/}
+                        <CustomButton className="mt-5" text="Start for free"/>
                     </div>
                     <div className="z-0 lg:translate-x-20 translate-x-5 max-lg:mt-5">
                         <Image src={"/img/img_3.png"} alt={"Private processing over other platforms"} width={808}
